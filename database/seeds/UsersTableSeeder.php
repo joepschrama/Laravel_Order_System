e80ad1d96a18a786729fbd4ae2606924bc6a9940
joepschrama <joepschrama@hotmail.com>
2019-04-02 14:50:46 +0200

    Remove bootstrap

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,9 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'henk',
-            'email' => 'henk@gmail.com',
-            'password' => bcrypt('henk'),
+            'name' => 'marc',
+            'email' => 'm@akkerman.com',
+            'password' => bcrypt('marc'),
         ]);
     }
 }
